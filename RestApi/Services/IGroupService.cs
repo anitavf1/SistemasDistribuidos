3fd173c7ee5e1b2139252e4a9107f593cdeb3a78
Ana María Velázquez Figueroa <134451821+anitavf1@users.noreply.github.com>
2024-11-05 17:04:12 -0600

    Feat/get by name async (#15)
    * feat: paginación y agregación
    
    * feat: Método para obtener por nombre exacto

--- RestApi/Services/IGroupService.cs
+++ RestApi/Services/IGroupService.cs
@@ -2,9 +2,18 @@
 using RestApi.Models;
 
 
+
 namespace RestApi.Services;
 
 public interface IGroupService {
     Task<GroupUserModel>GetGroupByIdAsync(string id, CancellationToken cancellationToken);
     Task<IList<GroupUserModel>>GetGroupByNameAsync(string name,int page, int pageS, string orderBy, CancellationToken cancellationToken); 
+
+    Task<IList<GroupUserModel>>GetGroupByExactNameAsync(string name,int page, int pageS, string orderBy, CancellationToken cancellationToken); 
+
+
+    Task DeleteGroupByIdAsync(string id, CancellationToken cancellationToken);
+
+    Task <GroupUserModel> CreateGroupAsync(string name, Guid [] Users, CancellationToken cancellationToken);
+
 }
